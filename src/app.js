48f287fd6bfb09d7565ba5a4a219a4697dab6644
Jordy-6 <jper220803@gmail.com>
2024-11-22 19:48:59 +0100

    Project initialization: back

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require ('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Connection has been etablished successfully');
+    })
+    .catch((error) => {
+        console.error('Unable to connect database: ', error);
+    });
+
+app.get('/', (req, res) => {
+    res.send('Test CV');
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
